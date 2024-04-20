e2fb20dd97aefa32b0b2b3f3a917b05418e20b09
Tibi <tiberiu.vas1@gmail.com>
2024-04-20 18:16:32 +0300

    Improved code for label cleaning

--- label_cleaning.py
+++ label_cleaning.py
@@ -8,5 +8,7 @@ def find_consecutive_digits(text):
     return match.group() if match else None
 
 df = pd.read_csv('naics_codes_stable.csv')
+df.drop('naics_label', axis=1, inplace=True)
 df['naics_code'] = df['naics_code'].apply(find_consecutive_digits)
+df.dropna(subset=['naics_code'], inplace=True)
 df.to_csv('cleaned_naics_codes.csv', index=False)
